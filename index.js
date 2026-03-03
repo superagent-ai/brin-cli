793d5a6a81239677f3e410fada2502368f6fa473
Ismail Pelaseyed <homanp@gmail.com>
2026-03-03 22:53:11 +0100

    chore: remove index.js, update package.json description and keywords

--- index.js
+++
@@ -1,8 +0,0 @@
-// This package provides the brin CLI binary
-// Use it via the command line: brin <command>
-// For documentation, visit: https://brin.sh
-
-module.exports = {
-  version: require('./package.json').version,
-  binaryPath: require('path').join(__dirname, 'bin', 'brin'),
-};
